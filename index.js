f8130826fdbbe53daebfe7891c7aa4138a44e49d
Virginia Dooley <vcdooley@gmail.com>
2018-09-12 13:49:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function yourAnimal() {
   // and the above function both pass?
   // P.S.: You can't just hard-code 'cat' below
   var yourAnimal = "cat";
-  return yourAmimal
+  return yourAnimal
 }
 
 function add2(n) {
